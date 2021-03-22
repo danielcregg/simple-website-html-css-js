77e8091a360f81e44ecc6480b5d3078315a7d000
Daniel Cregg <danielcregg@users.noreply.github.com>
2021-03-22 15:32:22 +0000

    Delete js directory

--- js/myScripts.js
+++
@@ -1,7 +0,0 @@
-function changeTextColor() {
-	if (document.getElementById("paratxt").style.color == "red") {
-		document.getElementById("paratxt").style.color = "blue";
-	} else {
-		document.getElementById("paratxt").style.color = "red";
-	}   
-}
